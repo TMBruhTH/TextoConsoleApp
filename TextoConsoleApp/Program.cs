dc848426a2618635e876bcb01452b8de598f3f84
TMBruhTH <brunno_1211@hotmail.com>
2022-11-22 15:30:40 -0300

    Add project files.

---
+++ TextoConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Digite o valor da primeira coluna: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o valor da segunda coluna: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+string[] lines = File.ReadAllLines(@"C:\Users\Brunn\OneDrive\Documentos\TextoApp\Usuarios.txt");
+
+Console.WriteLine();
+
+foreach (var item in lines)
+{
+    string[] split = item.Split(";");
+
+	if (num1 == Convert.ToInt32(split[0]) && num2 == Convert.ToInt32(split[1]))
+	{
+		Console.WriteLine(item);
+		return;
+	}
+}
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
